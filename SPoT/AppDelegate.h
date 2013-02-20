27292cbc5e5c71859e8b3893f63f57fe2133c511
zodester <mas08j@my.fsu.edu>
2013-02-20 13:29:12 -0500

    Initial Commit

---
+++ SPoT/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SPoT
+//
+//  Created by Michael Sena on 2/20/13.
+//  Copyright (c) 2013 seaDevelopment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SPoT/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SPoT
+//
+//  Created by Michael Sena on 2/20/13.
+//  Copyright (c) 2013 seaDevelopment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
